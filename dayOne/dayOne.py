ee042ba0f657b8ec694605f8a5466644b188bd0d
Carson Kegley <carson.kegley@gmail.com>
2021-12-01 15:07:52 -0500

    Day One Solution

---
+++ dayOne/dayOne.py
@@ -0,0 +1,28 @@
+from typing import Container
+
+
+file = open("./dayOne/input.txt", "r")
+
+content = file.read()
+content_list = content.split("\n")
+content_list_int = []
+depthIncreases = 0
+depthIncreasesTripple = 0
+
+# for element in content_list:
+#     content_list_int.append(int(element))
+
+for index in range(1,len(content_list)):
+    if content_list[index] > content_list[index-1]:
+        depthIncreases += 1 
+
+for index in range(0,len(content_list)-3):
+    primaryTripple = int(content_list[index]) + int(content_list[index+1]) + int(content_list[index+2])
+    secondaryTripple = int(content_list[index+1]) + int(content_list[index+2]) + int(content_list[index+3])
+    if primaryTripple < secondaryTripple:
+        depthIncreasesTripple += 1
+
+print(content_list)
+print(depthIncreases)
+print(depthIncreasesTripple)
+
