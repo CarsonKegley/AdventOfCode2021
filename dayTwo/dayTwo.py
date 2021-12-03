9b6a40e628cb8a191b37b3a1734b516b7cd39248
Carson Kegley <carson.kegley@gmail.com>
2021-12-02 21:35:07 -0500

    Day two done

--- dayTwo/dayTwo.py
+++ dayTwo/dayTwo.py
@@ -1,10 +1,37 @@
-file = open("./input.txt", "r")
+file = open("./dayTwo/input.txt", "r")
 
 content = file.read()
 content_list = content.split("\n")
-vector_list= []
+vector_list = []
+xValue = 0
+yValue = 0
 
+# Part one
 for line in content_list:
-    vector_list.append([line.split(" ")])
+    vector_list.append(line.split(" "))
 
-print(vector_list[0])
+for vector in vector_list:
+    if vector[0] == "forward":
+        xValue += int(vector[1])
+    elif vector[0] == "down":
+        yValue += int(vector[1])
+    elif vector[0] == "up":
+        yValue -= int(vector[1])
+
+print(xValue*yValue)
+
+# Part two
+aim = 0
+xValue = 0
+yValue = 0
+
+for vector in vector_list:
+    if vector[0] == "forward":
+        xValue += int(vector[1])
+        yValue += int(vector[1]) * aim
+    elif vector[0] == "down":
+        aim += int(vector[1])
+    elif vector[0] == "up":
+        aim -= int(vector[1])
+
+print(xValue*yValue)
