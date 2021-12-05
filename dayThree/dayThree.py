7febd0c293f04de885ad5a9307382441d79a2987
Carson Kegley <carson.kegley@gmail.com>
2021-12-05 16:44:57 -0500

    Day Three part one complete

--- dayThree/dayThree.py
+++ dayThree/dayThree.py
@@ -1,4 +1,5 @@
 import math
+from os import remove
 
 file = open("./dayThree/input.txt", "r")
 
@@ -6,7 +7,7 @@ content = file.read()
 content_list = content.split("\n")
 bitList = [0,0,0,0,0,0,0,0,0,0,0,0,]
 
-
+# part one
 for element in content_list:
     for index in range(0, len(element)):
         if element[index] == "1":
@@ -15,8 +16,24 @@ for element in content_list:
 bitGama = list(map(lambda x: math.ceil(math.tanh(x - 500)), bitList))
 bitEpsilon = list(map(lambda x: math.ceil(math.tanh((x *-1) +1)),bitGama))
 
-bitGama = ''.join(str(x) for x in bitGama)
-bitEpsilon = ''.join(str(x) for x in bitEpsilon)
-bitGama = int(bitGama, base=2)
-bitEpsilon = int(bitEpsilon, base=2)
-print(bitGama*bitEpsilon)
+bitGamaStr = ''.join(str(x) for x in bitGama)
+bitEpsilonStr = ''.join(str(x) for x in bitEpsilon)
+bitGamaInt = int(bitGamaStr, base=2)
+bitEpsilonInt = int(bitEpsilonStr, base=2)
+print(bitGamaInt*bitEpsilonInt)
+
+# part two
+oxegenRatingList = content_list.copy()
+
+print(bitGamaStr,bitEpsilonStr)
+for index in range(0,len(oxegenRatingList)):
+    for element in oxegenRatingList:
+        if element[index] != bitGama[index]:
+            oxegenRatingList.remove(element)
+
+
+print(bitList)
+oxegenRating = "001101001010"
+co2ScrubberRating = "110010110011"
+lifeSupportRating = int(oxegenRating,base=2) * int(co2ScrubberRating,base=2)
+print(lifeSupportRating)
